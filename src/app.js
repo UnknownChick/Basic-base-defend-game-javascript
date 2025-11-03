57d250d447df41bb452a19ccc75e4838ae12fc19
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2025-11-03 01:36:10 +0100

    feat: implement projectile update and animation logic

--- src/app.js
+++ src/app.js
@@ -35,10 +35,45 @@ class Projectile {
 		ctx.fillStyle = this.color;
 		ctx.fill();
 	}
+
+	update() {
+		this.draw();
+		this.x = this.x + this.velocity.x;
+		this.y = this.y + this.velocity.y;
+	}
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
+const projectiles = [];
+
+function animate() {
+	requestAnimationFrame(animate);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	player.draw();
+	projectiles.forEach((projectile) => {
+		projectile.update();
+		projectile.draw();
+	});
+}
+
+window.addEventListener('click', (event) => {
+	const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+	const velocity = {
+		x: Math.cos(angle),
+		y: Math.sin(angle)
+	};
+	projectiles.push(
+		new Projectile(
+			canvas.width / 2,
+			canvas.height / 2,
+			5,
+			'red',
+			velocity
+		)
+	);
+});
+
+animate();

20efaba144803eccba61e576690033b194ce64e1
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2025-11-03 01:00:58 +0100

    feat: add Projectile class with draw method for rendering projectiles

--- src/app.js
+++ src/app.js
@@ -20,6 +20,23 @@ class Player {
 	}
 }
 
+class Projectile {
+	constructor(x, y, radius, color, velocity) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.velocity = velocity;
+	}
+
+	draw() {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
